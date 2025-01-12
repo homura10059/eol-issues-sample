f501570fcec1e8042117ef96fa0e6bd1c0fedcb1
homura <preasper0+github@gmail.com>
2025-01-12 20:51:00 +0900

    init

---
+++ scripts/add-issues.ts
@@ -0,0 +1,11 @@
+// #!/usr/bin/env pnpm run ts
+
+const main = async () => {
+  const products = ['nodejs']
+
+  // https://endoflife.date/api/{product}.json を引いてデータとってくる
+
+  console.log('Hello, world!')
+}
+
+main()
